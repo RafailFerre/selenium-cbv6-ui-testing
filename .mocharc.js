2ea40c962ffd60156f9d4844f5ddc1d34e23e54f
Rafail Ferre <qa.rafailferre@gmail.com>
2024-07-14 17:05:15 -0400

    istall selenium-webdriver, chromedriver, mocha, chai, babel, prettier; create .babelrc, .mocharc.js, dir test, auth.spec.js

---
+++ .mocharc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    require: ['@babel/register'],
+    timeout: 30000,
+    exclude: [],
+    // file: 'setup/global.js',
+    // reporter: 'mochawesome',
+    // reporterOptions: [
+    //     'json=false',
+    //     'quiet=true',
+    //     'reportDir=reports',
+    //     'reportFilename=[status]_updatedReports',
+    // ],
+};
